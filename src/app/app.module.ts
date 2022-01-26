cd99212555f9c4af9c48cad9a8a3d28c136a8c6b
Cerebrado <nallar.juan@gmail.com>
2022-01-26 18:00:18 +0100

    6 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, CommonModule],
-  declarations: [AppComponent, HelloComponent],
+  declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
