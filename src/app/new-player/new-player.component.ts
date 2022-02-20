66adbc40ebc7963d5c64a0948d87c8339a3c00a4
Cerebrado <nallar.juan@gmail.com>
2022-02-20 13:28:10 +0100

    3 files added

---
+++ src/app/new-player/new-player.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-player',
+  templateUrl: './new-player.component.html',
+  styleUrls: ['./new-player.component.css'],
+})
+export class NewPlayerComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+  btnSaveNewPlayerClick() {}
+}
