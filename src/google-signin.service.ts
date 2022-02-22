bb290cc0b1d1e4a5314c2c2d1be35fd1fdaac48b
Cerebrado <nallar.juan@gmail.com>
2022-02-22 17:16:17 +0100

    2 files modified

--- src/google-signin.service.ts
+++ src/google-signin.service.ts
@@ -1,33 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Observable, ReplaySubject } from 'rxjs';
 
 @Injectable()
 export class GoogleSigninService {
-  private auth2: gapi.auth2.GoogleAuth
+  private auth2: gapi.auth2.GoogleAuth<gapi.auth2.GoogleUser>(1);
   private subject = new ReplaySubject();
-  constructor() { 
+  constructor() {
     gapi.load('auth2', () => {
       this.auth2 = gapi.auth2.init({
-        client_id: '416593747264-jh0j6t1rl2e0if4r63f7vhrvmn49hgqi.apps.googleusercontent.com'
+        client_id:
+          '416593747264-jh0j6t1rl2e0if4r63f7vhrvmn49hgqi.apps.googleusercontent.com',
+      });
+    });
+  }
+
+  public SignIn() {
+    this.auth2
+      .signIn()
+      .then((user) => {
+        this.subject.next(user);
       })
+      .catch(() => {
+        this.subject.next(null);
+      });
+  }
+
+  public SignOut() {
+    this.auth2.signOut().then(() => {
+      this.subject.next(null;
     });
   }
 
-  public SignIn(){
-    this.auth2.signIn(
-
-    ).then(user => {
-
-    }).catch(() => {
-
-    });
+  public observable(): Observable {
+    return this.subject.asObservable();
   }
-
-  public SignOut(){
-    this.auth2.signOut(
-
-    ).then(() => {
-
-    })
-  }
-
 }
